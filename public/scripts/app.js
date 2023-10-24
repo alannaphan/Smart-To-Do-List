5cd33c0cee1c8eff3d79b488f1765d9ab9f1ac73
lorraineC26 <lorraine.quinyee@gmail.com>
2023-10-24 19:01:38 -0400

    set up profile page; improved css

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,7 @@
 // Client facing scripts here
+
+
+
+
+
+
--- routes/users.js
+++ routes/users.js
@@ -10,6 +10,6 @@ const router  = express.Router();
 
 router.get('/', (req, res) => {
   res.render('users');
-}); 
+});
 
 module.exports = router;
