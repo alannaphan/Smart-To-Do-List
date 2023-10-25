3f473328265bdb04666d5d3b8cd20836a448dd71
Alanna <phan.alanna@gmail.com>
2023-10-24 20:12:35 -0400

    add one more category and update seed accordingly

--- db/seeds/02_categories.sql
+++ db/seeds/02_categories.sql
@@ -3,3 +3,4 @@ INSERT INTO categories (id, name) VALUES (1, 'To Read');
 INSERT INTO categories (id, name) VALUES (2, 'To Watch');
 INSERT INTO categories (id, name) VALUES (3, 'To Eat');
 INSERT INTO categories (id, name) VALUES (4, 'To Buy');
+INSERT INTO categories (id, name) VALUES (5, 'Other');
--- db/seeds/03_items.sql
+++ db/seeds/03_items.sql
@@ -2,7 +2,7 @@
 INSERT INTO items (user_id, category_id, name, deadline) VALUES (1, 1, 'Harry Potter', (DATE '2023-10-30'));
 INSERT INTO items (user_id, category_id, name, deadline) VALUES (1, 2, 'Barbie', (DATE '2023-11-05'));
 INSERT INTO items (user_id, category_id, name, deadline) VALUES (1, 3, 'Truffle Pasta', (DATE '2023-10-28'));
-INSERT INTO items (user_id, category_id, name, deadline) VALUES (1, 3, 'Peking Duck', (DATE '2023-11-15'));
+INSERT INTO items (user_id, category_id, name, deadline) VALUES (1, 5, 'Breath of The Wild', (DATE '2023-11-15'));
 INSERT INTO items (user_id, category_id, name, deadline) VALUES (2, 1, 'Business Proposal', (DATE '2023-11-02'));
 INSERT INTO items (user_id, category_id, name, deadline) VALUES (2, 2, 'Spy x Family Ch 88', (DATE '2023-10-30'));
 INSERT INTO items (user_id, category_id, name, deadline) VALUES (2, 3, 'Strawberry Crepe', (DATE '2023-10-27'));
