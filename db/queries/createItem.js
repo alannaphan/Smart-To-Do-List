5b778f1ba706af07d099ba1d06a847b6880897b6
Alanna <phan.alanna@gmail.com>
2023-10-25 19:20:21 -0400

    create query to create new list items

--- db/queries/createItem.js
+++ db/queries/createItem.js
@@ -3,17 +3,20 @@ const db = require('../connection');
 const getItemsByUser = (user_id) => {
   return db.query(`SELECT * FROM items WHERE user_id = $1`, [user_id])
   .then((res) => {
-    console.log(res);
+    // console.log(res);
     console.log(res.rows);
+    // console.log(res.rows[0]);
   })
 }
+
 const createItem = (user_id, category_id, name, deadline) => {
-  return db.query(`INSERT INTO items (user_id, category_id, name, deadline) VALUES ($1, $2, '$3', (DATE '$4'));`, [user_id, category_id, name, deadline])
+  return db.query(`INSERT INTO items (user_id, category_id, name, deadline) VALUES ($1, $2, $3, $4);`, [user_id, category_id, name, deadline])
     .then((res) => {
-      console.log(res);
+      // console.log(res);
       getItemsByUser(user_id)
     });
 };
 
-module.exports = { getUserByName };
+module.exports = { getItemsByUser, createItem };
 
+createItem(1, 3, 'avocado eggie toast', 2023-10-25);
