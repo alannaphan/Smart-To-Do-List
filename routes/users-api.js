17976cc93293a96e04cc88c237bd6f8d58c7ef23
lorraineC26 <lorraine.quinyee@gmail.com>
2023-10-26 11:26:44 -0400

    start user-update

--- routes/users-api.js
+++ routes/users-api.js
@@ -9,8 +9,20 @@ const express = require('express');
 const router  = express.Router();
 const userQueries = require('../db/queries/users');
 
+// router.get('/', (req, res) => {
+//   userQueries.getUsers()
+//     .then(users => {
+//       res.json({ users });
+//     })
+//     .catch(err => {
+//       res
+//         .status(500)
+//         .json({ error: err.message });
+//     });
+// });
+
 router.get('/', (req, res) => {
-  userQueries.getUsers()
+  userQueries.getUserByUserID()
     .then(users => {
       res.json({ users });
     })
