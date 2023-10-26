9a73fad4b725f7c8e432ab41c75d6b6740de6ff5
lorraineC26 <lorraine.quinyee@gmail.com>
2023-10-26 16:28:19 -0400

    fix loading issue

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,17 @@ const userQueries = require('../db/queries/users');
 // browse home page
 router.get('/', (req, res) => {
   const userID = req.cookies.userID;
-  
+  if (!userID) {
+    const templateVars = {userID, username: 'Username'};
+    return res.render('index', templateVars);
+  }
+
   userQueries.getUsernameByID(userID)
     .then(username => {
-      const templateVars = { userID, username };
+      const templateVars = { userID, username};
       return res.render('index', templateVars);
     });
 
 });
-
-
 
 module.exports = router;
